c17d9c006f8d66b7a18ccb3ea27fd7a6d08ccb5b
juliana-has <julianamendez032793@gmail.com>
2026-04-17 13:07:11 -0500

    Define parking structure and helper function
    Added parking structure and array definitions for vehicles.

--- parking.cpp
+++ parking.cpp
@@ -7,3 +7,18 @@ struct Vehiculo {
     char placa[10];
     int horaEntrada;
     int ocupado;
+Vehiculo parqueadero[FILAS][COLS];
+char mapa[FILAS][COLS];
+
+// Arreglo
+
+void copiarArreglo(char destino[], char origen[]) {
+    int i = 0;
+
+    while(origen[i] != '\0') {
+        destino[i] = origen[i];
+        i++;
+    }
+
+    destino[i] = '\0';
+}
